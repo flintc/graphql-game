3e49f1dc8590fbbe3d86f359bfe239affae48d49
C Flint <flintc27@gmail.com>
2019-11-10 18:26:33 -0500

    Updates

--- precache-manifest.a7b5fd4d93f0a53dd7ac209cc3f73854.js
+++ precache-manifest.a7b5fd4d93f0a53dd7ac209cc3f73854.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ec760fc8426b8ab0121b1bd5f79cc4d0",
+    "revision": "41358291420280ea2221577c9f2006dc",
     "url": "/graphql-game/index.html"
   },
   {
-    "revision": "cbc119507c95b00782bb",
+    "revision": "5ffc17ea4325f0fbf381",
     "url": "/graphql-game/static/css/main.2e9cc5c9.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/graphql-game/static/js/2.7e6cbed2.chunk.js"
   },
   {
-    "revision": "cbc119507c95b00782bb",
-    "url": "/graphql-game/static/js/main.d25a7d38.chunk.js"
+    "revision": "5ffc17ea4325f0fbf381",
+    "url": "/graphql-game/static/js/main.03f06e6e.chunk.js"
   },
   {
     "revision": "c9e20193fa096d0a4d9d",
