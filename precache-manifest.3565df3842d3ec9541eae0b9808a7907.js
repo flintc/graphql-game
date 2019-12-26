76a712dbde812da38c6d5cd77a03d13d47bde5a7
C Flint <flintc27@gmail.com>
2019-12-26 13:48:01 -0500

    Updates

---
+++ precache-manifest.3565df3842d3ec9541eae0b9808a7907.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dffe3cb2b2be7d22b2a3f045080b5c08",
+    "url": "/graphql-game/index.html"
+  },
+  {
+    "revision": "e1f2af71bd589b265842",
+    "url": "/graphql-game/static/css/main.e447c576.chunk.css"
+  },
+  {
+    "revision": "df55555a566fac54c5eb",
+    "url": "/graphql-game/static/js/2.7b23d59d.chunk.js"
+  },
+  {
+    "revision": "e1f2af71bd589b265842",
+    "url": "/graphql-game/static/js/main.bbc8006b.chunk.js"
+  },
+  {
+    "revision": "c9e20193fa096d0a4d9d",
+    "url": "/graphql-game/static/js/runtime~main.eb655c35.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/graphql-game/precache-manifest.678a6c36212a4d574adb0359f9586386.js"
+  "/graphql-game/precache-manifest.3565df3842d3ec9541eae0b9808a7907.js"
 );
 
 self.addEventListener('message', (event) => {
