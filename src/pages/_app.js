5d909d232c0399ed607c666c681e3d9df1eb985c
C Flint <flintc27@gmail.com>
2021-10-24 16:00:03 -0400

    force dark mode

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -104,26 +104,31 @@ function MyApp({ Component, pageProps }) {
             <script
               dangerouslySetInnerHTML={{
                 __html: `
-              if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
                 document.documentElement.classList.add('dark-theme')
-                document.documentElement.classList.add('dark')
-
-              } else {
-                document.documentElement.classList.remove('dark-theme')
-                document.documentElement.classList.remove('dark')
-
-              }
-
-              // Whenever the user explicitly chooses light mode
-              localStorage.theme = 'light'
-
-              // Whenever the user explicitly chooses dark mode
-              localStorage.theme = 'dark'
-
-              // Whenever the user explicitly chooses to respect the OS preference
-              localStorage.removeItem('theme')
               `,
               }}
+              // dangerouslySetInnerHTML={{
+              //   __html: `
+              // if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+              //   document.documentElement.classList.add('dark-theme')
+              //   document.documentElement.classList.add('dark')
+
+              // } else {
+              //   document.documentElement.classList.remove('dark-theme')
+              //   document.documentElement.classList.remove('dark')
+
+              // }
+
+              // // Whenever the user explicitly chooses light mode
+              // localStorage.theme = 'light'
+
+              // // Whenever the user explicitly chooses dark mode
+              // localStorage.theme = 'dark'
+
+              // // Whenever the user explicitly chooses to respect the OS preference
+              // localStorage.removeItem('theme')
+              // `,
+              // }}
             />
           </Head>
           <AnimateSharedLayout type="crossfade">
